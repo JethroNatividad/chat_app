0d0e5d1eeea77b5fad9851972440796a0221f0a3
JethroSama <kel73896@gmail.com>
2022-05-21 22:50:59 +0800

    implement generateUniqueNumber

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -1,5 +1,6 @@
-import { collection, CollectionReference, DocumentData } from "firebase/firestore"
+import { collection, CollectionReference, DocumentData, getDoc, setDoc } from "firebase/firestore"
 import { db } from "./firebase"
+import { lastUniqueNumberRef } from "./refs/User"
 
 export const createCollection = <T = DocumentData>(collectionName: string) => {
     return collection(db, collectionName) as CollectionReference<T>
@@ -8,4 +9,12 @@ export const createCollection = <T = DocumentData>(collectionName: string) => {
 export const getErrorMessage = (error: unknown): string => {
     if (error instanceof Error) return error.message
     return String(error)
+}
+
+export const generateUniqueNumber = async () => {
+    const lastUniqueNumber = await getDoc(lastUniqueNumberRef)
+    const data = lastUniqueNumber.data()
+    const newNumber = data ? data.LastUniqueNumber + 1 : 1
+    await setDoc(lastUniqueNumberRef, { LastUniqueNumber: newNumber })
+    return newNumber
 }
