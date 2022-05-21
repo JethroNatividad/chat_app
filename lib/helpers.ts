f326742f82a7613e8bffee7e694cea8e0cb32809
JethroSama <kel73896@gmail.com>
2022-05-21 18:37:28 +0800

    Add getErrorMessage helper

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -5,3 +5,7 @@ export const createCollection = <T = DocumentData>(collectionName: string) => {
     return collection(db, collectionName) as CollectionReference<T>
 }
 
+export const getErrorMessage = (error: unknown): string => {
+    if (error instanceof Error) return error.message
+    return String(error)
+}
