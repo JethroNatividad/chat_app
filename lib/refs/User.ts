4fc27b4440e8154cf15d8cf336ffdca29195d8a3
JethroSama <kel73896@gmail.com>
2022-05-21 18:17:06 +0800

    Add user refs

---
+++ lib/refs/User.ts
@@ -0,0 +1,9 @@
+import { CollectionReference, doc } from "firebase/firestore";
+import { User } from "../../types/User";
+import { createCollection } from "../helpers";
+
+export const usersRef = createCollection<User>('users')
+
+export const userRef = (uid: string) => {
+    return doc(usersRef, uid)
+}
