d52d60810617c6a2d9b8fdb4a9527ad0543911f7
JethroSama <kel73896@gmail.com>
2022-05-21 17:47:05 +0800

    Initialize firebase

---
+++ lib/firebase.ts
@@ -0,0 +1,14 @@
+import { FirebaseOptions, initializeApp } from "firebase/app";
+
+const firebaseConfig: FirebaseOptions = {
+    apiKey: "AIzaSyBhs3Zbso7WvJLsAngkvthSATSqn0nCrnw",
+    authDomain: "chat-app-cf870.firebaseapp.com",
+    projectId: "chat-app-cf870",
+    storageBucket: "chat-app-cf870.appspot.com",
+    messagingSenderId: "184700239809",
+    appId: "1:184700239809:web:4235e3bdee81426c203940",
+    measurementId: "G-M1Y49W3RJS"
+};
+
+const app = initializeApp(firebaseConfig);
+
