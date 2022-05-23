9ecae7f53980169c95f731ae30c5fd82700b51af
JethroSama <kel73896@gmail.com>
2022-05-23 20:18:32 +0800

    Add color pallette

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-dark': '#3C3F45',
+        'secondary-dark': '#2F3136',
+        'gray': {
+          '100': '#42464D', // sidebar hover
+          '200': '#3C3F45', // message hover
+          '300': '#32353B', // sidebar active
+          '400': '#2F3136',
+          '900': '#292B2F',
+        },
+
+      }
+    },
   },
   plugins: [],
 }
