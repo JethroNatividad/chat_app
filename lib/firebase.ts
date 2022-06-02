d9e7f7d7d179f38e4bca9573cd965f848a403cfd
Jethro <63900998+JethroSama@users.noreply.github.com>
2022-06-02 11:11:26 +0000

    firebase: change app config

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,14 +3,14 @@ import { collection, CollectionReference, DocumentData, getFirestore } from 'fir
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig: FirebaseOptions = {
-    apiKey: "AIzaSyBhs3Zbso7WvJLsAngkvthSATSqn0nCrnw",
-    authDomain: "chat-app-cf870.firebaseapp.com",
-    projectId: "chat-app-cf870",
-    storageBucket: "chat-app-cf870.appspot.com",
-    messagingSenderId: "184700239809",
-    appId: "1:184700239809:web:4235e3bdee81426c203940",
-    measurementId: "G-M1Y49W3RJS"
-};
+    apiKey: "AIzaSyCKVEatcjh2c2iJJrBIry_W45smW8tHJfM",
+    authDomain: "chat-app-a5f37.firebaseapp.com",
+    projectId: "chat-app-a5f37",
+    storageBucket: "chat-app-a5f37.appspot.com",
+    messagingSenderId: "754589067226",
+    appId: "1:754589067226:web:1066a58ad5ea22eb1682e3",
+    measurementId: "G-VYL8D95KG1"
+  };
 
 const app = initializeApp(firebaseConfig);
 
