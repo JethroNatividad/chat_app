965752707fdbfebd937112f2119125912c8eee14
JethroSama <kel73896@gmail.com>
2022-06-02 17:34:09 +0800

    Login component: Style header

--- components/Login.tsx
+++ components/Login.tsx
@@ -4,7 +4,14 @@ type Props = {}
 
 const Login = (props: Props) => {
     return (
-        <div>Login Page</div>
+        <div className='bg-secondary-dark h-screen w-screen'>
+            <div className='bg-primary-dark h-full w-full max-w-3xl mx-auto'>
+                <div className='shadow-sm shadow-secondary-dark py-5 px-10' >
+                    <h1 className='text-xl text-white font-semibold'>Sign in</h1>
+                </div>
+
+            </div>
+        </div >
     )
 }
 
