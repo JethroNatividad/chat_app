99cf4a089a23ab260797d56e86ee493ff78e178e
Jethro <63900998+JethroSama@users.noreply.github.com>
2022-06-03 09:49:00 +0000

    config toast

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,23 @@ import 'react-toastify/dist/ReactToastify.css';
 import type { AppProps } from 'next/app'
 import { ToastContainer } from 'react-toastify';
 
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <div>
     <Component {...pageProps} />
-    <ToastContainer />
+    <ToastContainer position="top-right"
+      autoClose={2000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss={false}
+      draggable
+      pauseOnHover={false}
+      toastClassName={
+        "relative flex bg-primary-dark shadow-lg p-2 min-h-10 rounded-md justify-between overflow-hidden cursor-pointer text-white"
+      }
+    />
   </div>
 }
 
