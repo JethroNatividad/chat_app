f0d9cfd931893dd4ee3c32923d347f62ed830674
Jethro <63900998+JethroSama@users.noreply.github.com>
2022-06-03 11:03:05 +0000

    Login: implement redirect on user

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { onAuthStateChanged } from 'firebase/auth'
+import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
 import Login from '../components/Login'
+import { auth } from '../lib/firebase'
 
-type Props = {}
+const LoginPage = () => {
+  const router = useRouter()
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) return router.push('/')
+    })
+    return () => {
+      unsubscribe()
+    }
+  }, [])
 
-const login = (props: Props) => {
   return (
     <div>
       <Login />
@@ -11,4 +22,4 @@ const login = (props: Props) => {
   )
 }
 
-export default login
+export default LoginPage
