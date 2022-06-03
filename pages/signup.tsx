2e72d3bfc05026983a400983c7d12f4de1525846
Jethro <63900998+JethroSama@users.noreply.github.com>
2022-06-03 11:03:16 +0000

    Signup: implement redirect on user

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { onAuthStateChanged } from 'firebase/auth'
+import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
 import Signup from '../components/Signup'
+import { auth } from '../lib/firebase'
 
 type Props = {}
 
 const signup = (props: Props) => {
+  const router = useRouter()
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) return router.push('/')
+    })
+    return () => {
+      unsubscribe()
+    }
+  }, [])
+
   return (
     <div>
       <Signup />
