483935d1769369e966f5ed830b45a2faae973850
JethroSama <kel73896@gmail.com>
2022-06-08 13:36:08 +0800

    Style chatitem

--- components/ChatItem.tsx
+++ components/ChatItem.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const ChatItem = ({ username, recentMessage }: Props) => {
     return (
-        <div className='flex space-x-2 items-center'>
+        <div className='flex space-x-2 items-center p-2 hover:bg-gray-100 cursor-pointer rounded-lg'>
             <div className='h-9 w-9 rounded-3xl bg-white' />
             <div className='flex flex-col justify-center'>
                 <p className='text-white font-semibold text-md'>{username}</p>
