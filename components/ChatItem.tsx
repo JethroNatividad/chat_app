bc728b758a5d880adcaf8ee62b611524a0f4f16d
JethroSama <kel73896@gmail.com>
2022-06-08 14:18:26 +0800

    implemement line clamp in chat item

--- components/ChatItem.tsx
+++ components/ChatItem.tsx
@@ -9,9 +9,9 @@ const ChatItem = ({ username, recentMessage }: Props) => {
     return (
         <div className='flex space-x-2 items-center p-2 hover:bg-gray-100 cursor-pointer rounded-lg'>
             <div className='h-9 w-9 rounded-3xl bg-white' />
-            <div className='flex flex-col justify-center'>
+            <div className='flex flex-col justify-center flex-1'>
                 <p className='text-white font-semibold text-md'>{username}</p>
-                <p className='text-white text-xs'>
+                <p className='text-white text-xs line-clamp-1'>
                     {recentMessage}
                 </p>
             </div>
