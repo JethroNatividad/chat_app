805da2389bac1bb478e3ffc5f55f9bbc1c4b7109
JethroSama <kel73896@gmail.com>
2022-06-10 19:10:49 +0800

    Implement search in searchbar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,9 +1,24 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { searchUsers } from '../lib/functions/user'
+import { User } from '../types/User'
 
 type Props = {}
 
 const SearchBar = (props: Props) => {
     const [searchText, setSearchText] = useState<string>('')
+    const [searchResults, setSearchResults] = useState<User[]>([])
+
+    useEffect(() => {
+        const fn = async () => {
+
+            if (searchText.length > 0) {
+                const results = await searchUsers(searchText)
+                return setSearchResults(results)
+            }
+            setSearchResults([])
+        }
+        fn()
+    }, [searchText])
 
     return (
         <div>
@@ -11,7 +26,7 @@ const SearchBar = (props: Props) => {
                 setSearchText(e.currentTarget.value)
             }} value={searchText} type='text' placeholder='Test' className='input bg-primary-dark text-white placeholder:text-white ring-gray-500' />
 
-            <p>{searchText}</p>
+            <p>{JSON.stringify(searchResults)}</p>
         </div>
     )
 }
