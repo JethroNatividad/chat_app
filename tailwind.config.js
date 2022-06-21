e47d0516fdd0da81e1932d80cbd6834717ccd612
JethroSama <kel73896@gmail.com>
2022-06-21 13:41:55 +0800

    Add color gray 500

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
           '200': '#3C3F45', // message hover
           '300': '#32353B', // sidebar active
           '400': '#2F3136',
+          '500': '#40444B', // text input
           '900': '#292B2F',
         },
 
