cfc24e0e19d86ddc1f3951196bde3294193e3c50
JethroSama <kel73896@gmail.com>
2022-06-21 13:27:27 +0800

    Check openchatgroupid

--- components/ChatView.tsx
+++ components/ChatView.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 
-type Props = {}
+type Props = {
+    openChatGroupId: string | null
+}
 
 const ChatView = (props: Props) => {
+    if (props.openChatGroupId === null) return (
+        <div className='h-full w-full bg-primary-dark flex items-center justify-center'>
+            <h1 className='text-white text-lg font-semibold'>Open or start a new conversation</h1>
+        </div>
+    )
     return (
-        <div>ChatView</div>
+        <div className='h-full w-full bg-primary-dark'>
+            <div>Header</div>
+        </div>
     )
 }
 
