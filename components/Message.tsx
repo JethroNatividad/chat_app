e6671d7f9f267623684c164f7610fae2f1d12429
JethroSama <kel73896@gmail.com>
2022-06-21 18:07:32 +0800

    show time in message

--- components/Message.tsx
+++ components/Message.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
 import { populateUserId } from '../lib/functions/user'
 import { User } from '../types/User'
+import moment from 'moment'
 
 type Props = {
     timestamp: Timestamp
@@ -21,6 +22,8 @@ const Message = ({ message, userId, timestamp }: Props) => {
         fn()
     }, [])
 
+
+
     return (
         <div className='flex space-x-2 px-5 py-1 hover:bg-gray-300 group'>
             <div className='h-9 w-9 rounded-3xl bg-white relative overflow-hidden' >
@@ -33,7 +36,7 @@ const Message = ({ message, userId, timestamp }: Props) => {
             <div className='flex-1'>
                 <div className='flex items-baseline space-x-2'>
                     <h1 className='text-white text-lg'>{user?.username}</h1>
-                    <p className='text-xs text-slate-300'>{ }</p>
+                    <p className='text-xs text-slate-300'>{moment(timestamp.toDate()).calendar()}</p>
                 </div>
                 <div className='text-slate-200'>{message}</div>
             </div>
