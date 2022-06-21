d4656322134f6af6565a8f8a0f8f448f36a7f951
JethroSama <kel73896@gmail.com>
2022-06-21 13:42:07 +0800

    ChatView: style input

--- components/ChatView.tsx
+++ components/ChatView.tsx
@@ -12,11 +12,24 @@ const ChatView = (props: Props) => {
         </div>
     )
     return (
-        <div className='h-full w-full bg-primary-dark'>
+        <div className='h-full w-full bg-primary-dark flex flex-col'>
+            {/* Header */}
             <div className='h-16 w-full shadow-md flex items-center px-5 justify-between'>
                 <h1 className='text-white text-xl font-semibold'>Username</h1>
                 <div className='h-10 w-10 text-white'>
                     <UserAddIcon />
+                </div>
+            </div>
+
+            {/* Messages */}
+            <div className='flex-1'>
+                Messages
+            </div>
+
+            {/* Chat input */}
+            <div className='h-16 px-5 flex items-center'>
+                <div className='bg-gray-500 h-10 shadow-lg w-full rounded-lg overflow-hidden flex px-2'>
+                    <input className='w-full h-full outline-none bg-inherit text-white placeholder:text-white' type='text' placeholder='Type a message' />
                 </div>
             </div>
         </div>
