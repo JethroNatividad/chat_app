9dff4e119ad0392e8e7e2c15bd8ff49636d65a5d
JethroSama <kel73896@gmail.com>
2022-06-21 14:12:37 +0800

    MessageComponent: style

--- components/Message.tsx
+++ components/Message.tsx
@@ -6,7 +6,13 @@ type Props = {
 
 const Message = (props: Props) => {
     return (
-        <div>Message</div>
+        <div className='flex space-x-2 px-5 py-1 hover:bg-gray-300'>
+            <div className='h-12 w-12 rounded-3xl bg-white relative overflow-hidden' />
+            <div className='flex-1'>
+                <h1 className='text-white text-lg'>Username</h1>
+                <div className='text-white'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto autem consectetur, quidem aperiam modi odit fuga ab! Fugit odit consequuntur animi at expedita! Neque deserunt aut sed, laborum blanditiis molestias nobis reprehenderit repellat enim deleniti, dolor distinctio iste illo est excepturi voluptatem reiciendis unde, laboriosam molestiae labore sequi ab nihil. Debitis, obcaecati provident nesciunt aliquam repellendus, hic minima facilis magni minus eligendi totam laboriosam non! Soluta vel, commodi molestiae, libero aliquid amet earum ea unde assumenda pariatur ab illum cupiditate expedita optio facere animi laudantium corporis. Vitae illo provident, eos blanditiis laboriosam perferendis commodi nesciunt enim quia molestiae ducimus. Deleniti illum fugit nisi saepe quisquam amet praesentium distinctio! Voluptatum excepturi adipisci nemo eveniet odit. Asperiores neque quia nisi, sunt ex dolor magni maxime quas quo nihil commodi. Dicta, consequatur, officia iure perferendis, doloribus adipisci incidunt a molestiae delectus quas rerum dolorem soluta. Doloribus nam aspernatur cum in commodi distinctio recusandae illo esse repudiandae! Assumenda maiores iure dicta accusantium minima accusamus corrupti qui possimus! Quam aspernatur officiis magnam nemo atque. Esse atque architecto itaque recusandae omnis sapiente reprehenderit, quod dolorum rerum quaerat delectus, corporis quisquam cupiditate earum reiciendis accusantium odit, libero molestiae consequuntur! Rem odit tempora laborum provident perferendis architecto et.</div>
+            </div>
+        </div>
     )
 }
 
