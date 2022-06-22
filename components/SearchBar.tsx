c924979217ef090ae4d15097ae3082d4d307966e
JethroSama <kel73896@gmail.com>
2022-06-22 14:57:27 +0800

    Searchbar: Implement Debounce effect

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -9,30 +9,39 @@ type Props = {}
 const SearchBar = (props: Props) => {
     const [searchText, setSearchText] = useState<string>('')
     const [searchResults, setSearchResults] = useState<User[]>([])
+    const [timeouts, setTimeouts] = useState<NodeJS.Timeout>()
 
     useEffect(() => {
         const fn = async () => {
-
-            if (searchText.length > 0) {
+            if (searchText !== '') {
                 const results = await searchUsers(searchText)
-                return setSearchResults(results)
+                if (results) return setSearchResults(results)
             }
-            setSearchResults([])
+            return setSearchResults([])
         }
-        fn()
+        clearTimeout(timeouts)
+
+        setTimeouts(setTimeout(() => {
+            fn()
+        }, 1000))
     }, [searchText])
+
+    const handleSearchTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchText(e.target.value)
+    }
+
+
 
     // {"uniqueNumber":2,"username":"Jethro","following":[],"email":"kel73896@gmail.com","followers":[],"uid":"GXn7G4cupjP5YwGXWUOkLCxwSPK2","chatGroups":[], profilePicture: ""}
     return (
         <div className='relative'>
-            <input onChange={(e) => {
-                setSearchText(e.currentTarget.value)
-            }} value={searchText} type='text' placeholder='Search users' className='input bg-primary-dark text-white placeholder:text-white ring-gray-500 w-full' />
+            <input onChange={handleSearchTextChange} value={searchText} type='text' placeholder='Search users' className='input bg-primary-dark text-white placeholder:text-white ring-gray-500 w-full' />
 
             <div className='absolute top-14 left-0 w-full z-10'>
                 {searchResults.map((user) => (
                     <UserSearchResult key={user.uid} profilePicture={user.profilePicture} username={user.username} uniqueNumber={user.uniqueNumber} userId={user.uid} />
                 ))}
+                {JSON.stringify(searchResults)}
             </div>
         </div>
     )
