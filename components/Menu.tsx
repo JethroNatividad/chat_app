5892d7a08ca8782d182333bf7172c7fb86669fdf
JethroNatividad <kel73896@gmail.com>
2023-02-28 22:00:16 +0800

    Add alt on all Images

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,53 +1,58 @@
-import React, { useEffect } from 'react'
-import { CogIcon, UsersIcon } from '@heroicons/react/solid'
-import ChatItem from './ChatItem'
-import useUser from '../hooks/useUser'
-import { ChatGroup } from '../types/Chats'
-import SearchBar from './SearchBar'
-import Image from 'next/image'
+import React, { useEffect } from "react";
+import { CogIcon, UsersIcon } from "@heroicons/react/solid";
+import ChatItem from "./ChatItem";
+import useUser from "../hooks/useUser";
+import { ChatGroup } from "../types/Chats";
+import SearchBar from "./SearchBar";
+import Image from "next/image";
 
 const Menu = () => {
-    const currentUser = useUser()
-    console.log(currentUser)
-    const chatGroupIds: string[] = currentUser?.chatGroups ?? []
-    return (
-        <div className='w-full flex flex-col justify-between h-full bg-secondary-dark'>
-            {/* Header */}
-            <div className='w-full flex flex-col justify-center px-2 shadow-md h-16'>
-                {/* <SearchBar openChatGroupId={openChatGroupId} setOpenChatGroupId={setOpenChatGroupId} /> */}
-            </div>
+	const currentUser = useUser();
+	console.log(currentUser);
+	const chatGroupIds: string[] = currentUser?.chatGroups ?? [];
+	return (
+		<div className="w-full flex flex-col justify-between h-full bg-secondary-dark">
+			{/* Header */}
+			<div className="w-full flex flex-col justify-center px-2 shadow-md h-16">
+				{/* <SearchBar openChatGroupId={openChatGroupId} setOpenChatGroupId={setOpenChatGroupId} /> */}
+			</div>
 
-            {/* Chat list */}
-            <div className='flex-1 p-2'>
-                {/* {chatGroupIds.map((chatGroupId) => (
+			{/* Chat list */}
+			<div className="flex-1 p-2">
+				{/* {chatGroupIds.map((chatGroupId) => (
                     <ChatItem key={chatGroupId} chatGroupId={chatGroupId} openChatGroupId={openChatGroupId} setOpenChatGroupId={setOpenChatGroupId} />
                 ))} */}
+			</div>
 
-            </div>
+			{/* User bar */}
+			<div className="w-full flex h-16 justify-between bg-gray-900 px-2">
+				<div className="flex h-full space-x-2 items-center justify-center w-fit">
+					<div className="h-10 w-10 rounded-3xl bg-white relative overflow-hidden">
+						{currentUser?.profilePicture ? (
+							<Image
+								alt={currentUser.username}
+								src={currentUser.profilePicture}
+								layout="fill"
+								objectFit="cover"
+							/>
+						) : null}
+					</div>
+					<div className="flex flex-1 justify-center items-center space-x-2">
+						<p className="text-white w-fit font-semibold text-md">
+							{currentUser?.username}
+						</p>
+						<p className="text-md font-semibold text-gray-500">
+							#{currentUser?.uniqueNumber}
+						</p>
+					</div>
+				</div>
+				<div className="flex items-center space-x-3">
+					<UsersIcon className="h-10 w-10 text-gray-50 cursor-pointer" />
+					<CogIcon className="h-10 w-10 text-gray-50 cursor-pointer" />
+				</div>
+			</div>
+		</div>
+	);
+};
 
-            {/* User bar */}
-            <div className='w-full flex h-16 justify-between bg-gray-900 px-2'>
-                <div className='flex h-full space-x-2 items-center justify-center w-fit'>
-                    <div className='h-10 w-10 rounded-3xl bg-white relative overflow-hidden'>
-                        {
-                            currentUser?.profilePicture ? (
-                                <Image src={currentUser.profilePicture} layout="fill" objectFit='cover' />
-                            ) : null
-                        }
-                    </div>
-                    <div className='flex flex-1 justify-center items-center space-x-2'>
-                        <p className='text-white w-fit font-semibold text-md'>{currentUser?.username}</p>
-                        <p className='text-md font-semibold text-gray-500'>#{currentUser?.uniqueNumber}</p>
-                    </div>
-                </div>
-                <div className='flex items-center space-x-3'>
-                    <UsersIcon className='h-10 w-10 text-gray-50 cursor-pointer' />
-                    <CogIcon className='h-10 w-10 text-gray-50 cursor-pointer' />
-                </div>
-
-            </div>
-        </div>
-    )
-}
-
-export default Menu
+export default Menu;
--- components/UserSearchResult.tsx
+++ components/UserSearchResult.tsx
@@ -1,39 +1,57 @@
-import Image from 'next/image'
-import React from 'react'
-import useUser from '../hooks/useUser'
-import { checkIfAlreadyInChatGroup, createChatGroup } from '../lib/functions/chats'
-import { User } from '../types/User'
+import Image from "next/image";
+import React from "react";
+import useUser from "../hooks/useUser";
+import {
+	checkIfAlreadyInChatGroup,
+	createChatGroup,
+} from "../lib/functions/chats";
+import { User } from "../types/User";
 
 type Props = {
-    profilePicture: string
-    username: string
-    uniqueNumber: number
-    userId: string
-    setOpenChatGroupId: (chatGroupId: string) => void
-    setSearchResults: (users: User[]) => void
-}
+	profilePicture: string;
+	username: string;
+	uniqueNumber: number;
+	userId: string;
+	setOpenChatGroupId: (chatGroupId: string) => void;
+	setSearchResults: (users: User[]) => void;
+};
 
-const UserSearchResult = ({ profilePicture, uniqueNumber, username, userId, setOpenChatGroupId, setSearchResults }: Props) => {
-    const handleClick = async () => {
-        const alreadyInChatGroup = await checkIfAlreadyInChatGroup(userId)
-        if (alreadyInChatGroup) {
-            setSearchResults([])
-            return setOpenChatGroupId(alreadyInChatGroup)
-        }
-        return await createChatGroup([userId])
-    }
+const UserSearchResult = ({
+	profilePicture,
+	uniqueNumber,
+	username,
+	userId,
+	setOpenChatGroupId,
+	setSearchResults,
+}: Props) => {
+	const handleClick = async () => {
+		const alreadyInChatGroup = await checkIfAlreadyInChatGroup(userId);
+		if (alreadyInChatGroup) {
+			setSearchResults([]);
+			return setOpenChatGroupId(alreadyInChatGroup);
+		}
+		return await createChatGroup([userId]);
+	};
 
-    return (
-        <div onClick={handleClick} className='flex bg-gray-100 cursor-pointer hover:bg-gray-200 px-3 py-2 items-center space-x-2 '>
-            <div className='h-10 w-10 rounded-3xl bg-white relative overflow-hidden'>
-                <Image src={profilePicture} layout="fill" objectFit='cover' />
-            </div>
-            <div className='flex items-center space-x-1'>
-                <p className='text-white font-semibold text-md'>{username}</p>
-                <p className='text-gray-50 font-semibold text-md'>#{uniqueNumber}</p>
-            </div>
-        </div>
-    )
-}
+	return (
+		<div
+			onClick={handleClick}
+			className="flex bg-gray-100 cursor-pointer hover:bg-gray-200 px-3 py-2 items-center space-x-2 "
+		>
+			<div className="h-10 w-10 rounded-3xl bg-white relative overflow-hidden">
+				<Image
+					alt={username}
+					src={profilePicture}
+					layout="fill"
+					objectFit="cover"
+				/>
+			</div>
+			<div className="flex items-center space-x-1">
+				<p className="text-white font-semibold text-md">{username}</p>
+				<p className="text-gray-50 font-semibold text-md">#{uniqueNumber}</p>
+			</div>
+		</div>
+	);
+};
 
-export default UserSearchResult
+export default UserSearchResult;
