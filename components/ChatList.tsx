b24b9057995dc15d4eac0ddd7682e54f0881c8c5
JethroSama <kel73896@gmail.com>
2023-03-19 22:40:48 +0800

    Show test data

--- components/ChatList.tsx
+++ components/ChatList.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { useChat } from '../context/ChatContext'
 
 type Props = {}
 
 const ChatList = (props: Props) => {
+    const { chatList, chatListLoading } = useChat()
     return (
-        <div>ChatList</div>
+        <div>{chatListLoading ? "Loading..." : JSON.stringify(chatList)}</div>
     )
 }
 
