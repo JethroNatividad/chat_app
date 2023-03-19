d6c34f5767874f6ed51d59917ed2deb21cb2f1fb
JethroSama <kel73896@gmail.com>
2023-03-19 21:57:47 +0800

    chatcontext: add chatlist

--- context/ChatContext.tsx
+++ context/ChatContext.tsx
@@ -3,25 +3,37 @@ import { createContext, useState, useEffect, useContext } from 'react'
 interface IChatContext {
     activeChat: any;
     setActiveChat: (value: any) => void;
+    chatList: any;
 }
 
 const ChatContext = createContext<IChatContext>({
     activeChat: null,
-    setActiveChat: () => { }
+    setActiveChat: () => { },
+    chatList: null,
 })
 
 const ChatProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     const [activeChat, setActiveChat] = useState<any>(null);
+    const [chatList, setChatList] = useState<any>(null);
 
     useEffect(() => {
         // do something when active chat changes
     }, [activeChat]);
 
+    useEffect(() => {
+        // Listener to get chat list
+
+    }, []);
+
     return (
-        <ChatContext.Provider value={{ activeChat, setActiveChat }}>
+        <ChatContext.Provider value={{ activeChat, setActiveChat, chatList }}>
             {children}
         </ChatContext.Provider>
     );
 };
+
+export function useChat() {
+    return useContext(ChatContext);
+}
 
 export default ChatProvider
