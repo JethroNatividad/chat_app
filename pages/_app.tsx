26772317071fb8cdfd43d36f8323d87f81ff280b
JethroSama <kel73896@gmail.com>
2023-03-19 21:57:37 +0800

    _app: Use chatcontext provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import AuthProvider from "../context/AuthContext";
 import { ChakraProvider } from "@chakra-ui/react";
+import ChatProvider from "../context/ChatContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<ChakraProvider>
 			<AuthProvider>
-				<Component {...pageProps} />
+				<ChatProvider>
+					<Component {...pageProps} />
+				</ChatProvider>
 			</AuthProvider>
 		</ChakraProvider>
 	);
