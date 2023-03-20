86e4883caf12726dc60c3135761abb50a6a375a4
JethroSama <kel73896@gmail.com>
2023-03-20 18:35:03 +0800

    Add chat in chatlist

--- components/ChatList.tsx
+++ components/ChatList.tsx
@@ -1,12 +1,18 @@
+import { Box } from '@chakra-ui/react'
 import React from 'react'
 import { useChat } from '../context/ChatContext'
+import Chat from './Chat'
 
 type Props = {}
 
 const ChatList = (props: Props) => {
     const { chatList, chatListLoading } = useChat()
     return (
-        <div>{chatListLoading ? "Loading..." : JSON.stringify(chatList)}</div>
+        <div>{chatListLoading ? "Loading..." : (
+            <Box>
+                {chatList.map((chat) => (<Chat {...chat} />))}
+            </Box>
+        )}</div>
     )
 }
 
