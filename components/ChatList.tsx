39098bd7c2cb95d0092b5e754c40e7c9add5d3c6
JethroNatividad <kel73896@gmail.com>
2023-03-28 11:01:19 +0800

    Use chatloading in chatlist

--- components/ChatList.tsx
+++ components/ChatList.tsx
@@ -1,19 +1,29 @@
-import { Box } from '@chakra-ui/react'
-import React from 'react'
-import { useChat } from '../context/ChatContext'
-import Chat from './Chat'
+import { Box } from "@chakra-ui/react";
+import React from "react";
+import { useChat } from "../context/ChatContext";
+import Chat, { ChatLoading } from "./Chat";
 
-type Props = {}
+type Props = {};
 
 const ChatList = (props: Props) => {
-    const { chatList, chatListLoading } = useChat()
-    return (
-        <div>{chatListLoading ? "Loading..." : (
-            <Box>
-                {chatList.map((chat) => (<Chat {...chat} key={chat.id} />))}
-            </Box>
-        )}</div>
-    )
-}
+	const { chatList, chatListLoading } = useChat();
+	return (
+		<div>
+			{chatListLoading ? (
+				<Box>
+					{[...Array(5)].map((_, i) => (
+						<ChatLoading key={i} />
+					))}
+				</Box>
+			) : (
+				<Box>
+					{chatList.map((chat) => (
+						<Chat {...chat} key={chat.id} />
+					))}
+				</Box>
+			)}
+		</div>
+	);
+};
 
-export default ChatList
+export default ChatList;
