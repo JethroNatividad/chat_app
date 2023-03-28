4295a522fff8f78212546e6bfbda38d6a3a5bc4f
JethroSama <kel73896@gmail.com>
2023-03-28 16:49:32 +0800

    Chatview: add header

--- components/ChatView.tsx
+++ components/ChatView.tsx
@@ -1,10 +1,21 @@
+import { Box, Flex, Text } from '@chakra-ui/react'
 import React from 'react'
+import { useChat } from '../context/ChatContext'
 
 type Props = {}
 
 const ChatView = (props: Props) => {
+    const { activeChat, activeChatLoading, chatList } = useChat()
+    const memberNames = activeChat?.members.map((member) => member.username).join(", ");
+
     return (
-        <div>ChatView</div>
+        <Box>
+            <Flex alignItems="center" px="5" shadow='lg' minH="14">
+                <Text fontSize="xl">
+                    {memberNames}
+                </Text>
+            </Flex>
+        </Box>
     )
 }
 
