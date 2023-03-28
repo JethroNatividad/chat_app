cd6d675bc9a6f74d7a6d15121b5c0a91483d9e0f
JethroSama <kel73896@gmail.com>
2023-03-28 17:12:54 +0800

    Chatview: add back button

--- components/ChatView.tsx
+++ components/ChatView.tsx
@@ -1,17 +1,30 @@
-import { Box, Flex, Text } from '@chakra-ui/react'
+import { Box, Flex, IconButton, Text } from '@chakra-ui/react'
 import React from 'react'
 import { useChat } from '../context/ChatContext'
+import { RxChevronLeft } from 'react-icons/rx'
 
 type Props = {}
 
 const ChatView = (props: Props) => {
-    const { activeChat, activeChatLoading, chatList } = useChat()
+    const { activeChat, activeChatLoading, chatList, setActiveChatId } = useChat()
     const memberNames = activeChat?.members.map((member) => member.username).join(", ");
 
     return (
         <Box>
-            <Flex alignItems="center" px="5" shadow='lg' minH="14">
-                <Text fontSize="xl">
+            <Flex alignItems="center" px='5' shadow='lg' minH="14">
+                <div
+                    className="flex md:hidden text-white cursor-pointer"
+                    onClick={() => setActiveChatId(null)}
+                >
+                    <IconButton
+                        icon={<RxChevronLeft />}
+                        variant="outline"
+                        colorScheme="teal"
+                        aria-label="Settings"
+                        fontSize='25'
+                    />
+                </div>
+                <Text className='px-5 md:px-5' fontSize="xl">
                     {memberNames}
                 </Text>
             </Flex>
