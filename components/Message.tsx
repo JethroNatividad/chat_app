90c1cd6cbb24082e0bf869297785b40a6ad0e8d8
JethroSama <kel73896@gmail.com>
2023-03-31 23:29:18 +0800

    create new message

--- components/Message.tsx
+++ components/Message.tsx
@@ -1,48 +1,36 @@
-import { FieldValue, Timestamp } from "firebase/firestore";
-import Image from "next/image";
-import React, { useEffect, useState } from "react";
-import { populateUserId } from "../lib/functions/user";
-import { User } from "../types/User";
-import moment from "moment";
+import { Flex, Image, Text } from '@chakra-ui/react';
+import { Timestamp } from 'firebase/firestore';
+import moment from 'moment';
+import React from 'react'
+import { User } from '../types/User';
 
 type Props = {
-	timestamp: Timestamp;
-	message: string;
-	userId: string;
-	profilePicture: string | null | undefined;
-	username: string;
-};
+    timestamp: Timestamp;
+    message: string;
+    sentBy: User | null;
+}
 
-const Message = ({
-	message,
-	userId,
-	timestamp,
-	profilePicture,
-	username,
-}: Props) => {
-	return (
-		<div className="flex space-x-2 px-5 py-1 hover:bg-gray-300 group">
-			<div className="h-9 w-9 rounded-3xl bg-white relative overflow-hidden">
-				{profilePicture ? (
-					<Image
-						src={profilePicture}
-						alt={username}
-						layout="fill"
-						objectFit="cover"
-					/>
-				) : null}
-			</div>
-			<div className="flex-1">
-				<div className="flex items-baseline space-x-2">
-					<h1 className="text-white text-lg">{username}</h1>
-					<p className="text-xs text-slate-300">
-						{moment(timestamp.toDate()).calendar()}
-					</p>
-				</div>
-				<div className="text-slate-200">{message}</div>
-			</div>
-		</div>
-	);
-};
+const Message = ({ sentBy, message, timestamp }: Props) => {
+    return (
+        <Flex>
+            {sentBy && (
+                <Image
+                    borderRadius="full"
+                    boxSize="46px"
+                    src={sentBy.profilePicture}
+                    alt="Profile Picture"
+                    mr="3"
+                />
+            )}
+            <Flex>
+                <Flex>
+                    <Text>{sentBy && sentBy.username}</Text>
+                    <Text>{moment(timestamp.toDate()).calendar()}</Text>
+                </Flex>
+            </Flex>
 
-export default Message;
+        </Flex>
+    )
+}
+
+export default Message
